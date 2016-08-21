35f76a2ea7a24e46b317b0b7af72cd28f8d72357
Charlie Hsieh <charlieofficial@gmail.com>
2016-08-21 12:32:31 +0800

    finish file writes

--- lib/app.rb
+++ lib/app.rb
@@ -1,3 +1,3 @@
 require_relative 'filepath'
-#require_relative 'fileopen'
+require_relative 'fileopen'
 require_relative 'sqlserver'
--- lib/fileopen.rb
+++ lib/fileopen.rb
@@ -1,16 +1,28 @@
 class Fileopen
-  def initialize(f=__FILE__)
+  attr_accessor :html_text
+  def initialize(f="/tmp/erp.html")
+    @output_html = f
+    @html_text = ""
   end
 
-  def iflock(lockfile="")
-    exit if lockfile == ""
-    f = File.open(lockfile, 'r')
-    if ! f.flock(File::LOCK_EX | File::LOCK_NB)
-      puts "Another instance is already running"
-      exit
+  def writefile
+    File.open(@output_html, 'w') do |f|
+      if ! f.flock(File::LOCK_EX | File::LOCK_NB)
+        puts "Another instance is already running"
+        exit
+      end
+      f.puts @html_text
     end
-    sleep 10
-    exit
   end
+  #def iflock(lockfile="")
+  #  exit if lockfile == ""
+  #  f = File.open(lockfile, 'r')
+  #  if ! f.flock(File::LOCK_EX | File::LOCK_NB)
+  #    puts "Another instance is already running"
+  #    exit
+  #  end
+  #  sleep 10
+  #  exit
+  #end
 
 end
